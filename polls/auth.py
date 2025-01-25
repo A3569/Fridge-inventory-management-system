b2a6e02177e1dbc30fdafcc78bd94edcbaa53f93
Yuk Yin Fung <163593025+A3569@users.noreply.github.com>
2025-01-25 04:19:49 +0000

    Delete polls directory

--- polls/auth.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth.models import User
-
-class NoPasswordBackend(ModelBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        try:
-            return User.objects.get(username=username)
-        except User.DoesNotExist:
-            return None 
